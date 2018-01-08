990a1c1f0be0a527caf6586910413d1911c7e6e9
George Murray <murrayge@mnstate.edu>
2018-01-07 18:36:56 -0600

    Add Project page

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
 const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-
 
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
+
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server log');
-    }
-  });
+  fs.appendFile('server.log', log + '\n');
   next();
 });
 
@@ -27,31 +24,39 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-return new Date().getFullYear()
+  return new Date().getFullYear();
 });
 
+hbs.registerHelper('headerMessage', () => {
+  return 'Hello World Header';
+});
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
+
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Welcome to my Node Website',
-    pageMessage: "Save the fat cats",
-    headerMessage: 'Hello World Header'
+    pageMessage: 'Save the fat cats'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    headerMessage: 'Hello World Header'
+    pageTitle: 'About Page'
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+      pageTitle: 'Projects'
+  });
+});
+
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
-    message: 'Error bad page'
+    errorMessage: 'Unable to handle request'
   });
 });
 
